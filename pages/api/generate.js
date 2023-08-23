f06e2e87702d7d90c1e38c9a631465f6d473537f
Rahul Padalkar <rahulnpadalkar@gmail.com>
2023-08-23 12:31:11 +0530

    MVP changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,18 +9,18 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message: "Something went wrong.",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const userInput = req.body.userInput || "";
+  if (userInput.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: "Please enter a valid input",
+      },
     });
     return;
   }
@@ -28,35 +28,21 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: `Generate a strong and memorable passwords using the text "${userInput}"`,
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+  } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
+      console.error(`Something went wrong.`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "Something went wrong.",
+        },
       });
     }
   }
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ConfettiExplosion from "react-confetti-explosion";
+
+const smallProps = {
+  force: 0.4,
+  duration: 2200,
+  particleCount: 30,
+  width: 400,
+};
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [result, setResult] = useState("");
+  const [userInput, setUserInput] = useState();
+  const [isExploding, setIsExploding] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -14,45 +25,79 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ userInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
+      setUserInput("");
+      setIsExploding(true);
+    } catch (error) {
       console.error(error);
-      alert(error.message);
     }
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>SafePassword - Generate safe passwords using AI</title>
+        <link rel="icon" href="/brain.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/brain.png" className={styles.icon} />
+        <h3>Generate memorable and safe passwords using AI</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="password"
+            placeholder="Enter a text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="✨ Generate password" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {isExploding && (
+          <ConfettiExplosion
+            {...smallProps}
+            onComplete={() => {
+              setIsExploding(false);
+            }}
+          />
+        )}
+        <div className={styles.result}>
+          <div className={styles.password}>
+            <div style={{ width: "70%" }}>{result}</div>
+            <div
+              style={{
+                marginLeft: "1.5rem",
+                background: "#10a37f",
+                padding: "0.3rem",
+                cursor: "pointer",
+                borderRadius: "0.3rem",
+                color: "White",
+                width: "30%",
+              }}
+              onClick={() => {
+                navigator.clipboard.writeText(result);
+                toast.success("Copied! 👍", {
+                  style: { background: "#10a37f" },
+                });
+              }}
+            >
+              Copy
+            </div>
+          </div>
+        </div>
       </main>
+      <ToastContainer position="bottom-center" theme="colored" />
     </div>
   );
 }
